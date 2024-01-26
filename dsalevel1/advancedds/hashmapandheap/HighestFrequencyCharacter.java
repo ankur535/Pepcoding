597cd5bfa82c315ff63684128ff0e2a12716b86e
Ankur Garg <51332395+ankur535@users.noreply.github.com>
2024-01-26 20:31:57 +0530

    Add files via upload

--- dsalevel1/advancedds/hashmapandheap/HighestFrequencyCharacter.java
+++ dsalevel1/advancedds/hashmapandheap/HighestFrequencyCharacter.java
@@ -52,4 +52,35 @@ public class HighestFrequencyCharacter {
 		 */
 
 	}
+	
+	/* Efficient Solution
+	 
+	 public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String str = in.nextLine();
+		
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		
+		char ch = ' ';
+		int count = 0;
+		
+		for (char c: str.toCharArray()) {
+			
+			if (hm.containsKey(c)) {
+				hm.put(c, hm.get(c) + 1);
+			} else {
+				hm.put(c, 1);
+			}
+			
+			if (hm.get(c) > count) {
+				ch = c;
+				count = hm.get(c);
+			}
+		}
+		
+		System.out.println(ch);
+		
+	}
+	 */
 }
